64879b53d431b7a6fe75276ae2a767398c68a42c
Marcin Skalski <skalskimarcin33@gmail.com>
2026-04-11 19:05:42 +0200

    ci(bindings): fix trailing whitespace in Task class
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -928,11 +928,11 @@ export namespace task {
 	    plan?: string;
 	    planCritique?: string;
 	    filePath: string;
-
+	
 	    static createFrom(source: any = {}) {
 	        return new Task(source);
 	    }
-
+	
 	    constructor(source: any = {}) {
 	        if ('string' === typeof source) source = JSON.parse(source);
 	        this.id = source["id"];
